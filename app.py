84d9452cfaf86f7bebb693656a2dfb6a6dcf27e4
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-25 22:12:05 -0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from kivy.lang import Builder
-from kivymd.app import MDApp
+from kivymd.app import MDApp, ThemeManager
 
 class Gerar:
     def __init__(self):
@@ -7,7 +7,12 @@ class Gerar:
 
 class GeradorQR(MDApp):
     def build(self):
+        theme_cls = ThemeManager()
+        theme_cls.primary_palette = "DeepPurple"
         return Builder.load_file('style.kv')
+    
+    def on_start(self):
+        self.fps_monitor_start()
 
 if __name__ == '__main__':
     GeradorQR().run()
