e5c8e252098ac85f0b48ae5e618f0b75ccc96218
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-29 16:23:59 -0300

    .

--- gerador.py
+++ gerador.py
@@ -1,26 +1,44 @@
 from kivymd.app import MDApp
-from kivymd.uix.screenmanager import ScreenManager
+from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
+from pyodbc import connect
 
 class LoginWin(MDScreen):
     ...
     
-class MainWin(MDScreen):
+class Gerador(MDScreen):
     ...
     
 class App(MDApp):
     def build(self):
-        # ['Red', 'Pink', 'Purple', 'DeepPurple', 'Indigo', 'Blue', 'LightBlue', 'Cyan', 'Teal', 
-        # 'Green', 'LightGreen', 'Lime', 'Yellow', 'Amber', 'Orange', 'DeepOrange', 'Brown', 'Gray', 'BlueGray']
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
-        return Builder.load_file('style.kv')
-    
-    def login(self):
-        self.server = self.root.ids.entryServer.text
-        self.user = self.root.ids.entryUser.text
-        self.pwd = self.root.ids.entryPwd.text
+        Builder.load_file('style.kv')
+
+        # Seta Telas
+        self.sm = MDScreenManager()
+        self.sm.add_widget(LoginWin())
+        self.sm.add_widget(Gerador())
         
+        # Seta temas
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Gray"
+
+        return self.sm
+    def con(self):
+        driver = '{SQL Server}'
+        str_c = f"DRIVER={driver};SERVER={self.server};UID={self.user};PWD={self.pwd}"
+        self.conn = connect(str_c)
+        
+    def login(self):
+        ids = self.root.get_screen('login').ids
+        self.server = str(ids.entryServer.text)
+        self.user = str(ids.entryUser.text)
+        self.pwd = str(ids.entryPwd.text)
+        if self.user == 'guilherme.breve' and self.pwd == '84584608Guilherme':
+            self.con()
+            self.root.current = 'gerador'
+        else:
+            print('Error')
+            
 if __name__ == '__main__':
     App().run()
