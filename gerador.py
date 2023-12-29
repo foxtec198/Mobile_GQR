8bab711ef0dd206e0dc30906228fecfffece5253
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-29 06:53:11 -0300

    .

--- gerador.py
+++ gerador.py
@@ -1,28 +1,9 @@
-from flet import *
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
-def GeradorQR(page: Page):
-    def login(e):
-        print('Login Realizado')
-        
-    page.title = 'Gerador QR'
-    page.padding = 70
-    
-    icon = Icon(name=icons.QR_CODE_SCANNER,size=100)
-    inServer = TextField(label="Servidor")
-    inUser = TextField(label="Usuário")
-    inPwd = TextField(label="Senha", password=True, can_reveal_password=True)
-    salvarUser = Checkbox(label='Salvar Usuário')
-    btnLogin = FilledButton('Login', icon='login', on_click = login)
-    
-    cardLogin = Container(
-        content=Column([icon, inServer, inUser, inPwd, salvarUser, btnLogin]),
-        bgcolor = 'Black',
-        width = 300,
-        height= 450,
-        border_radius=20,
-        padding = 20
-    )
-    page.add(Column([cardLogin]))
+class Gerador(MDApp):
+    def build(self):
+        return Builder.load_file('style.kv')
 
 if __name__ == '__main__':
-    app(GeradorQR)
+    Gerador().run()
