e229b82d0a02feafb282893e2c8ed71bd4021b15
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-30 19:19:45 -0300

    .

--- hotReload.py
+++ hotReload.py
@@ -1,12 +1,20 @@
 from kivymd.tools.hotreload.app import MDApp
 from kivy.lang import Builder
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
 
+class LoginWin(MDScreen):
+    ...
+    
 class Gerador(MDApp):
     KV_FILES = ['style.kv']
     DEBUG = True
     
     def build_app(self):
-        return Builder.load_file('style.kv')
+        Builder.load_file('style.kv')
+        self.sm = MDScreenManager()
+        self.sm.add_widget(LoginWin())
+        return self.sm
     
     def login(self):
         ...
